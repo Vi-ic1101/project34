6ce91ca69ba509f1a42ed1917dcb761eee1b6116
Vi-ic1101 <83502402+Vi-ic1101@users.noreply.github.com>
2021-07-15 19:57:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,147 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+//var box1, box2, box3,box4;
+var hero,hero2,monster,rope,ground;
+var bg="gamingbackground2.png"
+var bg2="superhero1.png"
+
+function preload() {
+  backgroundImg= loadImage(bg);
+  heroImg=loadImage(bg2)
+  getTime()
+}
+
+function setup() {
+  createCanvas(3000, 700);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(400, 700, 2200, 20);
+
+  hero = new Hero(400,500,200);
+  //hero2=new Hero(mouseX,mouseY)
+  rope = new Rope(hero.body,{ x:20, y:5 });
+  monster = new Monster(1100,70,300);
+
+  box1 = new Box(900, 100, 70, 70);
+  box2 = new Box(900, 100, 70, 70);
+  box3 = new Box(900, 100, 70, 70);
+  box4 = new Box(900, 100, 70, 70);
+  box5 = new Box(900, 100, 70, 70);
+
+  box6 = new Box(800, 100, 70, 70);
+  box7 = new Box(800, 100, 70, 70);
+  box8 = new Box(800, 100, 70, 70); 
+  box9 = new Box(800, 100, 70, 70);
+  box10 = new Box(800, 100, 70, 70);
+  box11 = new Box(800, 100, 70, 70);
+  box12 = new Box(800, 100, 70, 70);
+  box13 = new Box(800, 100, 70, 70);
+ 
+  box14 = new Box(700, 100, 70, 70);
+  box15 = new Box(700, 100, 70, 70);
+  box16 = new Box(700, 100, 70, 70);
+  box17 = new Box(700, 100, 70, 70);
+  box18 = new Box(700, 100, 70, 70);
+  box19 = new Box(700, 100, 70, 70);
+
+  box20 = new Box(600, 100, 70, 70);
+  box21 = new Box(600, 100, 70, 70);
+  box22 = new Box(600, 100, 70, 70);
+  box23 = new Box(600, 100, 70, 70);
+  box24 = new Box(600, 100, 70, 70);
+  box25 = new Box(600, 100, 70, 70);
+  box26 = new Box(600, 100, 70, 70);
+ 
+
+}
+
+function draw() {
+  if(backgroundImg)
+  background(backgroundImg);
+
+
+  Engine.update(engine);
+  ground.display();
+ 
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display()
+ box5.display();
+
+ box6.display();
+ box7.display();
+ box8.display();
+ box9.display()
+box10.display();
+box11.display();
+box12.display();
+box13.display();
+
+box14.display()
+box15.display();
+box16.display();
+box17.display();
+box18.display();
+box19.display()
+
+box20.display();
+box21.display();
+box22.display();
+box23.display();
+box24.display();
+box25.display();
+box26.display();
+
+  hero.display();
+  rope.display();
+  monster.display();
+  //console.log(hero)
+
+  
+}
+  function mouseDragged (){
+    Matter.Body.setPosition(hero.body,{x:mouseX,y:mouseY})
+  }
+  function mouseReleased (){
+   // if(keyCode=== RIGHT_ARROW){
+      rope.fly()
+    
+      
+    }
+     
+  //}
+  function keyPressed(){
+    if(keyCode===32){
+      rope.attach(hero.body)
+    }
+    if(keyCode=== UP_ARROW){
+   //  Matter.Body.applyForce(hero.body,{x:hero.body.position.x,y:hero.body.position.y},{x:30000,y:10000})
+     Matter.Body.setVelocity(hero.body,{x:hero.body.position.x,y:hero.body.position.y})
+    }
+  }
+  async function getTime(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+       var responseJSON=await response.json()
+      var datetime=responseJSON.datetime
+       var hours= datetime.slice(11,13)
+   
+       if(hours>=0600 && hours<=1900){
+         bg="gamingbackground2.png"
+       bg2="superhero1.png"
+       }else{
+         bg="gamingbackground1.jpg"
+         bg2="superhero2.png"
+       }
+       backgroundImg=loadImage(bg)
+       heroImg=loadImage(bg2)
+  console.log(hero, hours)
+      
+   }
+
